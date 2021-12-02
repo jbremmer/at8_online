48a06910514bd7af59ce1208b22db7f7f6b792d1
Jacob Bremmer <jacob.bremmer@gmail.com>
2021-12-02 18:37:05 +0100

    skco app changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,7 +14,7 @@
 
 <Header />
 
-<section class="text-gray-600 body-font">
+<section class="text-gray-600 body-font bg-purple-50">
 	<div class="container mx-auto flex px-5 py-16 md:flex-row flex-col items-center">
 		<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
 			<img class="object-cover object-center rounded" alt="hero" src={heroImage} />
@@ -37,7 +37,7 @@
 				>
 				<a
 					href="/login"
-					class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-pink-200 rounded text-lg"
+					class="ml-4 inline-flex text-gray-700 bg-warmGray-300 border-0 py-2 px-6 focus:outline-none hover:bg-pink-200 rounded text-lg"
 					>Login</a
 				>
 			</div>
